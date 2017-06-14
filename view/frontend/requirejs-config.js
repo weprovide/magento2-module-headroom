c5d4e7f5af13d94072cc3d21c5e7c8f7defba6c7
Jeroen Alewijns <jeroen@weprovide.nl>
2017-06-14 16:46:10 +0200

    .js removed

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
   paths: {
     'headroom': [
-      'https://npmcdn.com/headroom.js@0.9.3/dist/headroom.min.js',
+      'https://npmcdn.com/headroom.js@0.9.3/dist/headroom.min',
       'WeProvide_Headroom/vendor/headroom'
     ]
   }
