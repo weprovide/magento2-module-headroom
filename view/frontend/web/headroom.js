223d2a10a386dbe0c95374c6f32fa39347a315cc
Jeroen Alewijns <jeroen@weprovide.nl>
2017-06-15 09:10:28 +0200

    return headroom

--- view/frontend/web/headroom.js
+++ view/frontend/web/headroom.js
@@ -1,6 +1,8 @@
 define(['headroom'], function (Headroom) {
   return function (configuration, element) {
-    var headroom  = new Headroom(element, configuration);
+    var headroom = new Headroom(element, configuration);
     headroom.init();
+
+    return headroom;
   }
 });
